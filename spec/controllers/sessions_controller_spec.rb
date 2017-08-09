90f1edae4c1162c974ae385447a1f331e3b0fbcf
Paul Liu <paul.pliu87@gmail.com>
2017-08-08 19:27:16 -0700

    Add session controller test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+    describe 'GET #new' do
+        it 'render login page' do
+            get :new
+            expect(response).to render_template(:new)
+        end
 
+        it 'return new success' do
+            get :new
+            expect(response).to have_http_status(:success)
+        end
+    end
+
+    describe 'POST #create' do
+        before do
+            @user = User.create(username: "tom", password: "123", password_confirmation: "123")
+        end
+
+        it 'redirect to todos view page' do
+            post :create, session: {username: "Tom", password: "123"}
+            expect(response).to redirect_to("/users/#{@user.id}/todos")
+        end
+    end
 end
