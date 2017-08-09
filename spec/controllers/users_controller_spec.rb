65d8a6e24099fc4061c205ebcf60c7cdc61329b8
Paul Liu <paul.pliu87@gmail.com>
2017-08-08 17:14:12 -0700

    setup test cases

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    describe 'new' do
+    describe 'GET #new' do
         it 'render register page' do
+            get :new
             expect(response).to render_template(:new)
         end
+
+        it 'return new success' do
+            get :new
+            expect(response).to have_http_status(:success)
+        end
     end
-    
+
+    describe 'create' do
+        before do
+            @user = User.new
+        end
+        it 'redirect to todos view page' do
+            expect(response).to redirect_to("/users/#{@user.id}/todos")
+        end
+    end
 end
