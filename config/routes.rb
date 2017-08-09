632ae1b155d4ef09a027d6e8860c6a6d7fcfaeb9
Paul Liu <paul.pliu87@gmail.com>
2017-08-08 19:59:48 -0700

    changing state between login ad logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  get 'signup', to: 'user#new', as: 'signup'
-  # get 'login', to: 'sessions#new', as: 'login'
-  # get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   root 'home#index'
   resources :users
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,13 +14,28 @@ RSpec.describe SessionsController, type: :controller do
     end
 
     describe 'POST #create' do
-        before do
-            @user = User.create(username: "tom", password: "123", password_confirmation: "123")
+        before :each do
+            @user = User.create(username: "Tom", password: "123", password_confirmation: "123")
         end
 
         it 'redirect to todos view page' do
             post :create, session: {username: "Tom", password: "123"}
             expect(response).to redirect_to("/users/#{@user.id}/todos")
+        end
+
+        it 'registes a user_id in session' do
+            post :create, session: {username: "Tom", password: "123"}
+            expect(session[:user_id]).to equal(@user.id)
+        end
+    end
+
+    describe "DELETE #delete" do
+        before :each do
+            session[:user_id] = 1
+        end
+
+        it 'removes the user_id from session' do
+            expect(session[:user_id]).to equal(nil)
         end
     end
 end
