e1a4de6c3014f9ea5f668bcd8bf160047b02d663
Paul Liu <paul.pliu87@gmail.com>
2017-08-08 19:35:57 -0700

    add session controller to pass all the tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+    def new
+    end
+    def create
+        user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_url, notice: "Log In"
+        else
+            flash.now.alert = "username or password is incorrect"
+            render "new"
+        end
+    end
 end
